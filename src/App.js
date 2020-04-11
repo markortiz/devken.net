68c0ceb67c9e6a3eb34d7472bc15b86ed07e3714
markortiz <markkennethortiz@gmail.com>
2020-04-12 00:49:04 +0800

    chore: update content

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import React, { useEffect } from "react";
-import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
 
-import "particles.js/particles.js";
-import * as particlesConfig from "./assets/particlesjs-config.json";
-
 function App() {
-  useEffect(() => {
-    console.log(particlesConfig.default);
-    window.particlesJS("App-bg", particlesConfig.default, function() {
-      console.log("callback - particles.js config loaded");
-    });
 
-    console.log("mounted");
-  }, []);
+  const yearsOfExperience = () => {
+    const date = new Date();
+    const year = date.getFullYear();
+    const startingYear = 2012;
+    return year-startingYear;
+  }
 
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Hello, I'm Ken!</h1>
-        <p>Senior Frontend Engineer</p>
+      <div className="App-wrapper">
+        <header className="App-header">
+          <h1 className="App-title red">Hi there! <span className="">I’m Ken,</span></h1>
+        </header>
+        <main className="App-main">
+          <p>a Frontend Engineer with {yearsOfExperience()} years of professional experience.</p>
+          <p>I started coding when I was 15, creating CSS templates for my social media account, and that’s when I got hooked into coding. </p>
+          <p>Currently, I’m developing small to large scale applications like as simple as landing pages to extensive and complex applications using modern web technologies such as:</p>
+          <ul>
+            <li>Web Components</li>
+            <li>Progressive Web App</li>
+            <li>Progressive Web App</li>
+            <li>React</li>
+            <li>React Native</li>
+            <li>Redux</li>
+            <li>Polymer</li>
+            <li>LIT Html</li>
+          </ul>
+          <p>I’m always fascinated by micro-services and micro-frontends architecture for complex applications where I use containerization technologies such as Docker and Kubernetes</p>
+          <p>
+            A big fan of:
+            <ul>
+              <li>I write Clean Code so humans can understand it</li>
+              <li>Pushing performant code</li>
+              <li>Writing slick tests</li>
+              <li>Composing demos using Storybook</li>
+              <li>Automating boring and repetitive stuff</li>
+              <li>CI and CD </li>
+            </ul>
+          </p>
+          <p>
+            And oh, did I mention that I’m also trying to be a Fullstack Javascript developer?
+            If you wanna hire me, just reach me at my email 
+            <strong> <a class="red" href="mailto:markkennethortiz@gmail.com">markkennethortiz@gmail.com</a></strong>.
+          </p>
+        </main>
         <section className="App-links">
           <a
             className="App-link"
@@ -47,8 +75,7 @@ function App() {
             <i className="fa fa-envelope" aria-hidden="true"></i>
           </a>
         </section>
-      </header>
-      <div id="App-bg" className="App-bg"></div>
+      </div>
     </div>
   );
 }
