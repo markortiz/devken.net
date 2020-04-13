5a2578ba8ccd667765326a25589d2418b81db2a5
markortiz <markkennethortiz@gmail.com>
2020-04-14 02:24:36 +0800

    chore: add work item placeholder

--- src/components/Works/Works.js
+++ src/components/Works/Works.js
@@ -4,8 +4,18 @@ import "./Works.css";
 
 function Works() {
   return (
-    <main className="App-main">
-      <h1 className="App-title red">Work</h1>
+    <main className="Work">
+      <h1 className="App-title red">Sample Work</h1>
+
+      <a href="#animals" className="Work-item row">
+        <figure className="Work-figure col-xs-12 col-md-4">
+          <img src="https://placeimg.com/640/480/animals" alt="Animals"  />
+        </figure>
+        <article className="Work-description col-xs-12 col-md-8">
+          <h2 className="Work-title red">Google Chrome</h2>
+          <p>Google Chrome is a web browser developed by Google, released in 2008. Chrome is the world's most popular web browser today!</p>
+        </article>
+      </a>
     </main>
   );
 }
