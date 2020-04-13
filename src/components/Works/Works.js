b13c8ddc3cccf76001ce46717f818c7acab4e125
markortiz <markkennethortiz@gmail.com>
2020-04-13 15:23:51 +0800

    chore: add works component

---
+++ src/components/Works/Works.js
@@ -0,0 +1,13 @@
+import React from "react";
+// CSS
+import "./Works.css";
+
+function Works() {
+  return (
+    <main className="App-main">
+      <h1 className="App-title red">Work</h1>
+    </main>
+  );
+}
+
+export default Works;
