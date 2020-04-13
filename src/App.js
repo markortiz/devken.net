ae3e9b54be068f14fd2297b8637f3f096c13342d
markortiz <markkennethortiz@gmail.com>
2020-04-13 17:01:58 +0800

    chore: apply nav component

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  NavLink
 } from 'react-router-dom';
 // Components
 import Home from './components/Home';
+import Nav from './components/Nav';
 import Works from './components/Works';
 // CSS
 import './assets/css/normalizer.css';
@@ -14,36 +14,11 @@ import './assets/css/flexboxgrid.css';
 import './App.css';
 
 function App() {
-
-  const isActive = (match, location) => {
-      console.log(match, location)
-      return match;
-  }
-
   return (
     <Router>
       <div className="App">
         <div className="App-wrapper">
-          <header className="App-header">
-            <nav className="App-menu">
-              <NavLink 
-                className="link" 
-                activeClassName="active"
-                isActive={isActive}
-                exact={true}
-                to="/">
-                  Home
-              </NavLink>
-              <NavLink 
-                className="link" 
-                activeClassName="active"
-                isActive={isActive}
-                exact={true}
-                to="/works">
-                  My Works
-              </NavLink>
-            </nav>
-          </header>
+          <Nav />
           <Switch>
             <Route exact path="/">
               <Home />
